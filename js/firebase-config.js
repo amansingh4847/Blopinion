657d87de1a5e60fdd009335b43a1eddf8f0b1d78
NOT-PRO-AMAN <amansingh484748@gmail.com>
2026-01-31 15:08:29 +0530

    Completelyfixed the register page

---
+++ js/firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCsWjyFGCWbeq23SG_xSXDsmkOdRL69nUY",
+  authDomain: "blopinion-a62b5.firebaseapp.com",
+  projectId: "blopinion-a62b5",
+  storageBucket: "blopinion-a62b5.firebasestorage.app",
+  messagingSenderId: "807528898085",
+  appId: "1:807528898085:web:915a3ed2b537bf905fa9cc"
+};
